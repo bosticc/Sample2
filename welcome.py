60c0b232b2edc72d10f1742685c14bcb6f1f2c57
bosticc <mbostic32@gmail.com>
2020-05-21 12:26:37 -0400

    Printing out some basic python commands

---
+++ welcome.py
@@ -0,0 +1,7 @@
+print("Welcome to my repo!")
+
+print("If I could, I think I would try to make this nice and snazzy, but unfortunately,")
+
+print("My abilities pretty much stop here. I think I will upload some projects that")
+
+print("I have done over the years though. I think that would be pretty cool.")
